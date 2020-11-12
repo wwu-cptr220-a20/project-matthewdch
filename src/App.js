2c68e9e4ec07fc51d98ed574f65dacc644b536b4
William.Hensel <william.r.hensel@gmail.com>
2020-11-11 17:43:52 -0800

    Fixed router

--- src/App.js
+++ src/App.js
@@ -15,21 +15,21 @@ export default class App extends Component {
           <header>
             <h1>The Game Shelf</h1>
             <nav>
-                <a href="index.html" type="button" class="btn btn-dark">Home</a>
-                <a href="info.html" type="button" class="btn btn-dark">About</a>
-                <a href="catalog.html" type="button" class="btn btn-dark">Catalog</a>
+              <Link to='/'>
+                <button className="btn btn-dark">Home</button>
+              </Link>
+              <Link to='/info'>
+                <button className="btn btn-dark">About</button>
+              </Link>
+              <Link to='/catalog'>
+                <button className="btn btn-dark">Catalog</button>
+              </Link>
             </nav>
            </header>
           <Switch>
-            <Route path="/index.html">
-              <Home />
-            </Route>
-            <Route path="/info.html">
-              <About />
-            </Route>
-            <Route path="/catalog.html">
-              <About />
-            </Route>
+            <Route exact path="/" component={Home}/>
+            <Route path="/info" component={About} />
+            <Route path="/catalog" component={Catalog} />
           </Switch>
         </div>
         <footer>
@@ -85,7 +85,74 @@ class Catalog extends Component {
   render() {
     return (
       <main>
-        
+        <section className="content">
+            <div className="container mt-4 mb-4">
+                <label for="boardGameInput">Enter a board game</label>
+                <div className="form-inline">
+                    <input type="text" className="form-control" id="boardGameInput"/>
+                    <button type="button" className="btn btn-success ml-4" id="submit-button">Submit</button>
+                    <button type="button" className="btn btn-primary ml-4" data-toggle="modal" data-target="#searchModal" id="search-modal-open-button">Search</button>
+                </div>
+            </div>
+            <div className="container mt-4 mb-4">
+                <table className="table table-dark">
+                    <tbody id="game-list">
+                    </tbody>
+                </table>
+            </div>
+            <div className="container mt-4 mb-4">
+                <h1>How to</h1>
+                <ul>
+                    <li>
+                        <p>
+                            Enter the name of a board game to add to your collection in the input field and click submit
+                            to add a new game.
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Click on the star rating on the game to change it.
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Click on the search button to bring up the search box.
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <div className="modal fade" id="searchModal" tabindex="-1" role="dialog" aria-labelledby="searchModalLabel" aria-hidden="true">
+            <div className="modal-dialog" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h2 className="modal-title" id="searchModalLabel">Search</h2>
+                  <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body">
+                    <div className="container mt-4 mb-4">
+                        <label for="boardGameInput">Search for a game</label>
+                        <div className="form-inline">
+                            <input type="text" className="form-control" id="searchInput"/>
+                            <button type="button" className="btn btn-success ml-4" id="search-button" disabled="true14">Search</button>
+                        </div>
+                    </div>
+                    <div className="container mt-4 mb-4">
+                        <table className="table">
+                            <tbody id="game-search-list">
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="button" className="btn btn-primary" id="search-submit-button" data-dismiss="modal">Submit</button>
+                </div>
+              </div>
+            </div>
+          </div>
       </main>
     )
   }
